933af86ca8f557fd676c048490df23f3896d0938
Your Name <you@example.com>
2023-01-04 08:02:19 +0700

    first commit_NVA025

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NGUYENVANA025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
